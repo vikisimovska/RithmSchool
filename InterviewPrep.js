f7304326b0d7634f816ae902be3a0b311006ec64
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-11 23:42:04 -0700

    ex.1

---
+++ InterviewPrep.js
@@ -0,0 +1,11 @@
+//system("clear")
+#1.
+function appendToString(str1, str2){
+  return str1 + str2;
+}
+
+appendToString("Hello", " World!"); // "Hello World!"
+appendToString("Foo", "bar"); // "Foobar"
+appendToString("bar", "Foo"); // "barFoo"
+appendToString("", "test"); // "test"
+appendToString("other test", ""); // "other test"
