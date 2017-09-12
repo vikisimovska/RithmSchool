b7e2966967b9c2c08d451d35683a37ac54006e86
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-11 23:43:12 -0700

    ex.2

--- InterviewPrep.js
+++ InterviewPrep.js
@@ -1,11 +1,20 @@
 //system("clear")
-#1.
+//1.
 function appendToString(str1, str2){
   return str1 + str2;
 }
 
-appendToString("Hello", " World!"); // "Hello World!"
-appendToString("Foo", "bar"); // "Foobar"
-appendToString("bar", "Foo"); // "barFoo"
-appendToString("", "test"); // "test"
-appendToString("other test", ""); // "other test"
+// appendToString("Hello", " World!"); // "Hello World!"
+// appendToString("Foo", "bar"); // "Foobar"
+// appendToString("bar", "Foo"); // "barFoo"
+// appendToString("", "test"); // "test"
+// appendToString("other test", ""); // "other test"
+
+//2.
+function prependToString(str1, str2){
+return (str2 + str1)
+}
+
+prependToString('awesome','very') // 'veryawesome'
+prependToString('world','hello ') // 'hello world'
+prependToString('nothing','') // 'nothing'
