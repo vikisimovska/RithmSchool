70b1a5211593f3cdcca09d25e22a69f492e361e6
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-15 20:09:43 -0700

    ex.26

--- InterviewPrep.js
+++ InterviewPrep.js
@@ -399,3 +399,19 @@ all([1,2,3,4,5], function(val){
 // }) // false
 
 //26.
+function any(arr, fn){
+  for (var i=0; i<arr.length; i++){
+    if (fn(arr[i]) === true)
+      return true;
+  }
+  return false;
+}
+
+// function any(arr,cb){
+//   for(var i = 0; i < arr.length; i++){
+//     if(cb(arr[i])) return true;
+//   }
+//   return false;
+// }
+
+//27.
