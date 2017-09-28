2e3830b1b44ba3753b87d681202177bf2b7c0586
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-27 17:54:08 -0700

    sm change

--- InterviewPrep.js
+++ InterviewPrep.js
@@ -15,7 +15,7 @@ function prependToString(str1, str2){
 return (str2 + str1)
 }
 
-// prependToString('awesome','very') // 'veryawesome'
+// prependToString('awesome','very ') // 'veryawesome'
 // prependToString('world','hello ') // 'hello world'
 // prependToString('nothing','') // 'nothing'
 
