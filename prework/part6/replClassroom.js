97e6533cd8bdd1c10f10177b2ef3114a0dfec5c2
Violeta Simovska <vikisimovska27@gmail.com>
2017-09-29 21:17:27 -0700

    replClassroom

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -1,17 +1,6 @@
-//1.Write a function called rangeInArray which accepts an array and start and end indices, 
-//and returns the sum of the values between (and including) the start and end index. 
-//If a start parameter is not passed in, it should default to zero. If an end parameter is not 
-//assed in, it should default to the last value in the array. Also, if the end argument is too 
-//arge, the sum should still go through the end of the array.
+//git clean same as rm, when u still havent add the new file
+//Git is a Version Control System. 
+//GitHub is an online platform for hosting and sharing code, 
+//text files and even more complex file formats.
 
-function rangeInArray(arr, num1=0, num2=arr[arr.length-1]){
-  if (num2 > arr.length-1) num2 = arr.length-1;
-  var sum = 0;
-  for (var i = num1; i <= num2; i++){
-    sum += arr[i];  
-  }
-  return sum;
-}
-
-//2.
-
+//1.Write a function called rangeInArray which accepts an array and start and end indices, ls
