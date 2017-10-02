359c93e0459e4bf327990c28248ddc8efe69a546
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-01 22:07:50 -0700

    ex.4

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -91,7 +91,14 @@ function take(arr, n){
     return arr.slice(0,1)
 }
 
-//4.
+//4.Write a function called includes which accepts a collection, a value, and an optional starting index. 
+//he function should return true if the value exists in the collection when we search starting from the 
+//starting index. Otherwise, it should return false.
+
+//The collection can be a string, an array, or an object. If the collection is a string or array, the third
+// parameter is a starting index for where to search from. If the collection is an object, the function 
+//searches for the value among values in the object; since objects have no sort order, the third parameter
+//is ignored.
 
 function includes(collection, value, optionalIndex){
   if (Array.isArray(collection) || typeof collection === 'string'){
