f7946cd73fa19b34c0e6af08bad9e04eecbd073f
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-11 00:50:19 -0700

    ex.15

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -282,6 +282,20 @@ function repeat(str, num){
   return newStr;
 }
 
+//15. Write a function called padEnd, which accepts a string and a number and pads the string on the 
+//right side if it's shorter than the length. By default, it will pad the string with whitespace. 
+//However, you can also provide a third argument to padEnd which will specify how it should pad the 
+//output string.
+
+//Padding characters are truncated if there's not enough padding, and are repeated if there's too much 
+//adding.
+
+//If the number is less than the input string length, no padding or truncation occurs; instead, the 
+//original string is simply returned.
+
+
+
+
 
 
 
