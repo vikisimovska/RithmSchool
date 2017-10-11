70373cf2cdad7f1a8f2789ffa1748414c199406e
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-10 21:58:40 -0700

    ex.9

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -197,7 +197,13 @@ function filter(arr, callback){
   return newArr;
 }
 
-//.9
+//9.Write a function called map which accepts an array and a callback. The function 
+//should iterate over the array passed to the function and return a new array, whose 
+//values are determined by the return values from the callback. For each iteration, the 
+//callback should accept the current value, current index and the entire original array 
+//as its arguments.
+
+//Since map returns a new array, it should not mutate the original array passed into it.
 
 
 
