733fbc0418714e40037fa7afa71244c0006541e6
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-11 00:53:18 -0700

    ex.16

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -336,7 +336,12 @@ function padEnd(str, num, char){
     return str.concat(newStr);
 }
 
+//16. Write a function called drop which accepts an array and optional number and returns a new array with 
+//the number  of elements dropped from the beginning. If the number is greater than the length of the array,
+// return an empty array and if there is no second parameter, return a slice starting from the first index 
+//to the end of the array.
 
+//Since drop returns a new array, it should not mutate the original array.
 
 
 
