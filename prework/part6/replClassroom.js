2402d835b08e99ca2d4213d833f90a16efa6a1ce
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-12 12:20:33 -0700

    ex.19

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -382,6 +382,16 @@ function findIndex(arr, cb){
   return -1;
 }
 
+//19.Write a function called linearSearch which accepts an array and a value, and returns the index at which
+// the value exists. If the value does not exist in the array, return -1.
+
+function linearSearch(arr, value){
+  for (let i=0; i<arr.length; i++){
+    if(value === arr[i]) return i;
+  }
+  return -1;
+}
+
 
 
 
