128015040218d891ca2c33fe83ac35ab2d07dac4
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-21 15:07:15 -0700

    ex.42

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -899,7 +899,29 @@ function remove(arr,cb){
   return newArr;
 }
 
-//42.
+//42.Write a function called sumBy which accepts an array of objects and a rule for summing up those objects to obtain
+// a value. The rule can be provided in one of two ways. One way is to provide a callback function which is invoked
+//or each element in array to generate the value to be summed. The callback is invoked with one argument: the current array element.
+
+//Another option for the rule is to pass in a string referencing a key. With this syntax, the value to be summed is the
+// value corresponding to the key passed in.
+
+function sumBy(arrOfObj, rule){
+  let sum = 0;
+  for (let i=0; i<arrOfObj.length; i++){
+    if(typeof rule === 'string'){
+      for(var key in arrOfObj[i] ){
+        if (key === rule) sum += arrOfObj[i][key];
+      }
+    }else{
+      if (rule(arrOfObj[i])) sum += rule(arrOfObj[i]);
+      }
+  }
+  return sum;
+}
+
+
+
 
 
 
