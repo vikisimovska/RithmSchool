3db64ff103c99e25544493f0710bde9da9cd286d
Violeta Simovska <vikisimovska27@gmail.com>
2017-10-20 21:36:24 -0700

    ex.38

--- prework/part6/replClassroom.js
+++ prework/part6/replClassroom.js
@@ -775,6 +775,29 @@ function letterCounter(str){
   }
 }
 
+//38.Write a function called omitBy which accepts an object and a callback function. This function should return 
+//an object with all the keys and values for which the values return falsey values when passed into the callback function.
+function omitBy(obj, cb){
+  let result = {};
+  for (let key in obj){
+    if (!cb(obj[key])) result[key] = obj[key];
+  }
+  return result;
+}
+
+//calling the function
+
+function isNumber(val) {
+  return Number.isFinite(val);
+}
+var isString(val) {
+  return typeof val === "string";
+}
+
+var object = { 'a': 1, 'b': '2', 'c': 3 };
+omitBy(object, isNumber) // { 'b': '2' }
+omitBy(object, isString) // { 'a': 1, 'c': 3 }
+
 
 
 
