93256c58ff4b5ef4fd165327daa455a62abafa8b
Violeta Simovska <vikisimovska27@gmail.com>
2017-11-26 16:12:55 -0800

    added 2 array obj

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -5,7 +5,8 @@
 
 // If the argument passed in is not a number, the function should return NaN.
 // If the argument passed in is less than 1, the function should return 0.
-// If the argument passed in is not a whole number, the function should add up all the whole numbers between 1 and the largest whole number less than the argument passed in.
+// If the argument passed in is not a whole number, the function should add 
+//up all the whole numbers between 1 and the largest whole number less than the argument passed in.
 
 function addUpTo(num) {
   var sum = 0;
@@ -27,4 +28,44 @@ function addUpTo(num) {
 // addUpTo(5.9999999);  // 15
 
 
-//2.
+//2.Part I
+
+//Write a function called twoArrayObject which accepts to arrays of varying 
+//lengths.The first array consists of keys and the second one consists of values. 
+//Your function should return an object created from the keys and values. 
+//If there are not enough values, the rest of keys should have a value of null. 
+//If there not enough keys, just ignore the rest of values.
+
+function twoArrayObject(keyArr, valArr){
+  var obj = {};
+  for (let i=0; i < keyArr.length; i++){
+    obj[keyArr[i]] = valArr[i] || null;
+  }
+  return obj;
+}
+
+twoArrayObject(['a', 'b', 'c', 'd'], [1, 2, 3]) // {'a': 1, 'b': 2, 'c': 3, 'd': null}
+twoArrayObject(['a', 'b', 'c']  , [1, 2, 3, 4]) // {'a': 1, 'b': 2, 'c': 3}
+twoArrayObject(['x', 'y', 'z']  , [1,2]) // {'x': 1, 'y': 2, 'z': null}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
