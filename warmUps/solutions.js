0d5da01def931f5e865135073dc2931e24b5b2b9
Violeta Simovska <vikisimovska27@gmail.com>
2018-01-04 18:42:04 -0800

    adding count pairs

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -365,4 +365,22 @@ truncate("Yo", 100) // "Yo"
 truncate("Super cool", 3) // "..."
 truncate("Super cool", 2) // "Truncation must be at least 3 characters."
 
+//Given an array of integers, and a number, find the number of pairs of integers in the array whose sum is equal
+// to the second parameter. You can assume that there will be no duplicate values in the array.
+
+function countPairs(arr, n) {
+ var count = 0;
+ for (let i=0; i<arr.length-1; i++){
+   for (let j=i+1; j < arr.length; j++){
+     if (arr[i] + arr[j] === n) count++;
+   }
+ }
+  return count;
+}
+countPairs([3,1,5,4,2], 6) // 2 (1,5 and 2,4)
+countPairs([10,4,8,2,6,0], 10) // 3 (2,8, 4,6, 10,0)
+countPairs([4,6,2,7], 10) // 1 (4,6)
+countPairs([1,2,3,4,5], 10) // 0
+countPairs([1,2,3,4,5], -3) // 0
+countPairs([0,-4],-4) // 1
 
