e0e147cc265354a82d28fd429d2faf2707e156f1
Violeta Simovska <vikisimovska27@gmail.com>
2018-01-04 18:46:35 -0800

    areThereDuplicates

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -421,6 +421,15 @@ function countPairs(arr, num){
   return count;
 }
 
+//Implement a function called, areThereDuplicates which accepts a variable number of arguments,
+// and checks whether there are any duplicates among the arguments passed in. 
+//solution with set
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
+}
+areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true 
+// areThereDuplicates('a', 'b', 'c', 'a') // true 
 
 
 
