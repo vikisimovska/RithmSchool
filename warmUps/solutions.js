e293ce72d61cfbd63e0ac1d9303a0fa09cdf377a
Violeta Simovska <vikisimovska27@gmail.com>
2018-01-14 18:35:16 -0800

    bitArraySort

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -636,6 +636,32 @@ function findPair(arr,num){
   return false;
 }
 
+//Write a function called bitArraySort which accepts an array of integers containing only 
+//0's and 1's returns the same array back in sorted order. You can assume that all inputs will be valid. 
 
+//This should be done in place as well so be mindful of the following constraints below:
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+
+function bitArraySort(arr) {
+  let left = 0;
+  let right = arr.length-1;
+  while(left<right){
+    if(arr[left]===1 && arr[right]===0){
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+    }else if(arr[left]===0){
+      left++;
+    }else{
+      right--;
+    }
+  }
+  return arr;
+}
+bitArraySort([0, 0, 0]) // [0, 0, 0]
+bitArraySort([1, 1, 1, 1]) // [1, 1, 1, 1]
+bitArraySort([0, 1, 1, 0, 1, 1, 0]) // [0, 0, 0, 1, 1, 1, 1]
+bitArraySort([1, 1, 1, 0, 1]) // [0, 1, 1, 1, 1]
+bitArraySort([1, 0, 0, 0, 0]) // [0, 0, 0, 0, 1]
+bitArraySort([1, 0, 1, 0, 0]) // [0, 0, 0, 1, 1]
 
 
