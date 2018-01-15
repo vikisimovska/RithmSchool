df703707e5ded431ff07346cbd4e20f4552ceba3
Violeta Simovska <vikisimovska27@gmail.com>
2018-01-14 18:32:14 -0800

    findPair

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -580,6 +580,32 @@ constructNote('aa', 'abc') // false
   return true;
 }
 
+//Given an unsorted array and a number n, find if there exists a pair of elements in the 
+//array whose difference is n. This function should return true if the pair exists or false if it does not.
+
+function findPair(arr, num){
+  s = new Set(arr)
+  if (!arr.length) return false;
+  for(let i=0; i<arr.length; i++){
+    lookUp = arr[i]-num;
+    if (s.has(lookUp)){
+      return true;
+    } 
+  }
+  return false
+}
+findPair([6,1,4,10,2,4], 2) // true
+findPair([8,6,2,4,1,0,2,5,13],1) // true
+findPair([4,-2,3,10],-6) // true
+findPair([6,1,4,10,2,4], 22) // false
+findPair([], 0) // false
+findPair([5,5], 0) // true
+findPair([-4,4], -8) // true
+findPair([-4,4], 8) // true
+findPair([1,3,4,6],-2) // true
+findPair([0,1,3,4,6],-2) // true
+
+//
 
 
 
