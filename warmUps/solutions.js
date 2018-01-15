0cd89f3235829d4061ac3f94a261b2f05686c1d4
Violeta Simovska <vikisimovska27@gmail.com>
2018-01-14 18:38:20 -0800

    characterFrequencySort

--- warmUps/solutions.js
+++ warmUps/solutions.js
@@ -681,3 +681,68 @@ function bitArraySort(arr) {
   }
   return arr;
 }
+//Write a function called characterFrequencySort which accepts a string and sorts it in decreasing order 
+//based on the frequency of characters and returns a new string. 
+//The most frequent character should appear first with its frequency, followed by the second most frequent 
+//character with its frequency, and so on. You can assume that all of the letters passed to the function 
+//will be lowercased and each letter will appear a different number of times.
+
+//Time Complexity - O(n log n)
+//Bonus Time Complexity - O(n)
+
+function characterFrequencySort(s) {
+    
+    let characters = new Set();
+    let freqMap = {};
+    let charStore = {};
+    let letters = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        characters.add(s[i]);
+        freqMap[s[i]] = (freqMap[s[i]] || 0) + 1
+    }
+    
+    for (let char of characters) {
+        if (!charStore[freqMap[char]]) charStore[freqMap[char]] = [];
+        charStore[freqMap[char]].push(new Array(freqMap[char]+1).join(char));
+    }
+    
+    for (let i = s.length; i > 0; i--) {
+        if (charStore[i]) {
+            letters = letters.concat(charStore[i]);
+        }
+    }
+    
+    return letters.join('');
+}
+characterFrequencySort("ttttrreee") // "tttteeerr"
+characterFrequencySort("cacac") // "cccaa"
+characterFrequencySort("eeeeffalll") // "eeeelllffa"
+characterFrequencySort("abbcccdddd") // "ddddcccbba"
+characterFrequencySort("xyzxyyzzzxyyyyzzzzz") // "zzzzzzzzzyyyyyyyxxx"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
